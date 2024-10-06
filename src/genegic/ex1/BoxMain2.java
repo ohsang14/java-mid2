518e057e2b68b6d0c017705b460ec163ed2e63f9
ohsang14 <ast6108@naver.com>
2024-10-06 15:46:03 +0900

    다형성 중복 해결 시도

--- src/genegic/ex1/BoxMain2.java
+++ src/genegic/ex1/BoxMain2.java
@@ -3,13 +3,20 @@ package genegic.ex1;
 public class BoxMain2 {
 
     public static void main(String[] args) {
-        ObjectBox objectBox = new ObjectBox();
-        objectBox.set(10);
-
-        Integer integer = (Integer) objectBox.get(); // control + t
-
+        ObjectBox integerBox = new ObjectBox();
+        integerBox.set(10);
+        Integer integer = (Integer) integerBox.get(); // control + t
+        // Object -> Integer 캐스팅
         System.out.println("integer = " + integer);
 
+        ObjectBox stringBox = new ObjectBox();
+        stringBox.set("hello");
+        String str = (String) stringBox.get(); // Object -> String 캐스팅
+        System.out.println("str = " + str);
 
+        // 잘못된 타입의 인수 전달시
+        integerBox.set("문자1000");
+        Integer result =(Integer) integerBox.get(); // String -> Integer 캐스팅 예외
+        System.out.println("result = " + result);
     }
 }
