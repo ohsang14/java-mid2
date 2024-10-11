0258277ec4ec82af4867b2f07e2ca2c82ba545cf
ohsang14 <ast6108@naver.com>
2024-10-11 20:02:28 +0900

    제네릭 적용

---
+++ src/genegic/ex1/RowTypeMain.java
@@ -0,0 +1,12 @@
+package genegic.ex1;
+
+public class RowTypeMain {
+    public static void main(String[] args) {
+        GenericBox integerBox = new GenericBox();
+        // GenericBox<Object> integerBox = new GenericBox<>();
+        integerBox.set(10);
+        Integer result = (Integer) integerBox.get();
+        System.out.println("result = " + result);
+
+    }
+}
