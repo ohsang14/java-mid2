73c37a02d798048b7963dc2af6e78092b6635c38
ohsang14 <ast6108@naver.com>
2024-10-14 02:00:25 +0900

    제네렉 도입과 실패

--- src/generic/ex3/AnimalHospitalV2.java
+++ src/generic/ex3/AnimalHospitalV2.java
@@ -1,22 +1,21 @@
 package generic.ex3;
 
-import generic.animal.Animal;
+public class AnimalHospitalV2<T> {
 
-public class AnimalHospitalV1 {
+    private T animal;
 
-    private Animal animal;
-
-    public void set(Animal animal) {
+    public void set(T animal) {
         this.animal = animal;
     }
 
     public void checkup() {
+        // T의 타입을 메서드를 정의하는 시점에는 알 수 없다. Object의 기능만 사용
         System.out.println("동물 이름:" + animal.getName());
         System.out.println("동물 크기:" + animal.getSize());
         animal.sound();
     }
 
-    public Animal bigger(Animal target) {
+    public T bigger(T target) {
         return animal.getSize() > target.getSize() ? animal : target;
     }
 }
